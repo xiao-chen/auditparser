5ae9c65fad5ba018cab8d84bc03039dcf66901c1
Xiao Chen <xiao@cloudera.com>
2017-10-29 21:56:38 -0700

    use spark 2.2.0

--- src/main/java/com/cloudera/experiment/JavaWordCount.java
+++ src/main/java/com/cloudera/experiment/JavaWordCount.java
@@ -35,7 +35,7 @@ import scala.Tuple2;
  */
 public class JavaWordCount {
 
-  private static final Pattern SPACE = Pattern.compile(",|:");
+  private static final Pattern DELIM = Pattern.compile(",|:");
 
   public static void main(String[] args) {
 
@@ -48,26 +48,11 @@ public class JavaWordCount {
     JavaSparkContext ctx = new JavaSparkContext(sparkConf);
     JavaRDD<String> lines = ctx.textFile(args[0], 1);
 
-    JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-      @Override
-      public Iterable<String> call(String s) {
-        return Arrays.asList(SPACE.split(s));
-      }
-    });
+    JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(DELIM.split(s)).iterator());
 
-    JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-      @Override
-      public Tuple2<String, Integer> call(String s) {
-        return new Tuple2<String, Integer>(s, 1);
-      }
-    });
+    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
 
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-      @Override
-      public Integer call(Integer i1, Integer i2) {
-        return i1 + i2;
-      }
-    });
+    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
 
     List<Tuple2<String, Integer>> output = counts.collect();
     for (Tuple2<?,?> tuple : output) {

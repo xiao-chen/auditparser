d41940c926809a61215e2335397dac55a3b15de9
Xiao Chen <xiao@cloudera.com>
2018-01-03 15:39:10 -0800

    add basic time based aggregation

--- src/main/scala/com/cloudera/experiment/AuditLogJsonAnalyser.scala
+++ src/main/scala/com/cloudera/experiment/AuditLogJsonAnalyser.scala
@@ -18,13 +18,18 @@
 
 package com.cloudera.experiment
 
+import java.text.SimpleDateFormat
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FSDataOutputStream, FileStatus, FileSystem, Path}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.functions._
-import scala.collection.mutable.Stack
 
+import scala.collection.mutable.Stack
+import org.slf4j.LoggerFactory
+
+//spark2-submit --class com.cloudera.experiment.AuditLogJsonAnalyser --deploy-mode client --master yarn --num-executors 6 --executor-cores 2 --executor-memory 1G ~/logparser-1.0.jar /audits/0925
 
 /**
   * Class to analyse audit logs in Navigator-compatible json format.
@@ -33,6 +38,7 @@ import scala.collection.mutable.Stack
   *   spark2-submit --class com.cloudera.experiment.AuditLogJsonAnalyser --deploy-mode client --master yarn ~/logparser-1.0.jar <filename>
   */
 object AuditLogJsonAnalyser {
+  val LOG = LoggerFactory.getLogger("AuditLogJsonAnalyser")
 
   // Navigator fields
   val ALLOWED = "allowed"
@@ -57,7 +63,7 @@ object AuditLogJsonAnalyser {
     }
     spark = SparkSession
       .builder
-      .appName("SparkKMeans")
+      .appName("AuditLogJsonAnalyser")
       .getOrCreate()
 
     var fs = FileSystem.get(new Configuration())
@@ -68,6 +74,8 @@ object AuditLogJsonAnalyser {
     outFile = fs.create(path)
 
     val inputPathName = args(0)
+    val interval = args(1)
+    // TODO: validate interval
     val inputPath = new Path(inputPathName)
     var status = fs.getFileStatus(inputPath)
     var dirs : Stack[FileStatus] = Stack()
@@ -76,7 +84,7 @@ object AuditLogJsonAnalyser {
       var iter = fs.listStatusIterator(path)
       while (iter.hasNext()) {
         status = iter.next();
-        println("____ processing " + status)
+        LOG.info("Processing " + status.getPath)
         if (status.isFile) {
           analyseFile(status.getPath)
         } else if (status.isDirectory) {
@@ -85,6 +93,7 @@ object AuditLogJsonAnalyser {
       }
     }
 
+    LOG.info("Analysing audits from {}", inputPathName)
     if (status.isDirectory) {
       processDir(inputPath)
       while (dirs.nonEmpty) {
@@ -101,33 +110,68 @@ object AuditLogJsonAnalyser {
   }
 
   def analyseFile(file: Path): Unit = {
-    writeToOutput("\n\n**** Analyzing file " + file.toString + " ****\n")
+    writeToOutput("\n\n**** File " + file.toString + ":\n")
+    LOG.info("Analyzing file {}", file)
     assert(spark != null)
     try {
-      val df = spark.read.json(file.toString)
-      analyzeColumn(USERNAME, df)
-      analyzeColumn(IP_ADDRESS, df)
-      analyzeColumn(OPERATION, df)
+      val dfCache = spark.read.json(file.toString).select(USERNAME, IP_ADDRESS, OPERATION, SRC, DEST, EVENT_TIME)
+      LOG.info("Successfully read file")
+      dfCache.cache()
+      LOG.info("Successfully cached file")
+      analyzeColumn(USERNAME, dfCache)
+      analyzeColumn(IP_ADDRESS, dfCache)
+      analyzeColumn(OPERATION, dfCache)
+      aggregateByTime(1000, dfCache)
     } catch {
-      case ae: AnalysisException => println("**** filed to analyze file " + file.toString)
+      case ae: AnalysisException => LOG.error("Failed to analyze file " + file.toString)
       case e : Exception => throw e
     }
   }
 
   def analyzeColumn(colName : String, df: DataFrame) : Unit = {
-    writeToOutput("\n**** Counting by " + colName + "****\n")
-    var result = df.select(USERNAME, IP_ADDRESS, OPERATION, SRC, DEST, EVENT_TIME)
-      .groupBy(colName).count().orderBy(desc("Count"))
-
-    result.show(false)
+    writeToOutput("\n**** Counting by " + colName + ":\n")
+    LOG.info("Analyzing column {}", colName)
+    var result = df.groupBy(colName).count().orderBy(desc("Count"))
     writeToOutput(result.rdd.map(_.mkString("|")).collect().mkString("\n"))
     writeToOutput("\n\n")
+  }
+
+  def aggregateByTime(interval : Int, df: DataFrame) : Unit = {
+    writeToOutput("\n**** Aggregation per " + interval / 1000 + " second:\n")
+    var startTime : Long = 0
+    var boundary : Long = 0
+    var count = 0;
+    val iter = df.toLocalIterator()
+    while (iter.hasNext) {
+      val eventTime : Long = iter.next().getAs(EVENT_TIME)
+      if (startTime == 0) {
+        startTime = eventTime - (eventTime % interval)
+        boundary = startTime + interval
+        LOG.info(s"____ eventTime=$eventTime, starttime=$startTime, boundary=$boundary")
+      }
+      count += 1
+      if (eventTime > boundary) {
+        writeToOutput(timeToStr(boundary) + "|" + count + "\n");
+        boundary = eventTime - (eventTime % interval) + interval
+        count = 0
+        LOG.info(s"____ eventTime=$eventTime, boundary=$boundary, readable={}", timeToStr(boundary))
+      }
+    }
+
+//    var result = df.groupBy(EVENT_TIME).count().orderBy(desc("Count")).limit(20)
+//    writeToOutput("\n**** Aggregation:\n")
+//    writeToOutput(result.rdd.map(_.mkString("|")).collect().mkString("\n"))
+//    writeToOutput("\n\n")
+    LOG.info("Aggregation done on per {} seconds", interval / 1000)
   }
 
   def writeToOutput(content : String): Unit = {
     assert(outFile != null)
     outFile.writeBytes(content)
   }
+
+  def timeToStr(epochMillis: Long): String =
+    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(epochMillis)
 
   def getOutputLocation() : String = {
     "hdfs:///result_" + System.currentTimeMillis()
